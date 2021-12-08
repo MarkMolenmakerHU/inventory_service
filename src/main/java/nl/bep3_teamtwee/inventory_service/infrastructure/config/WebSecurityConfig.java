88ff9d32c314e192d9e24b9c61c4dcd1dbbb1c13
markmolenmaker <mark.molenmaker@student.hu.nl>
2021-12-08 12:36:12 +0100

    Added WebSecurityConfig and Spring pom.xml dependency

---
+++ src/main/java/nl/bep3_teamtwee/inventory_service/infrastructure/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package nl.bep3_teamtwee.inventory_service.infrastructure.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+    }
+}
